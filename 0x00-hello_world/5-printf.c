a1293c295112e7ba9ed8da646ef2fa5805b60e37
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-02-16 22:56:47 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+
+
 /**
- * Entry - main
- * Return 0 always
+ * main - Entry point
+ *
+ * Return: Always 0(sucess)
  */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
+
 }
