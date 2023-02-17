e7d95604f035002d8cb7efb6856aa6c0df236d66
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-02-16 23:02:50 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+
 /**
- * Entry - main
- * Return 0 always
+ * main - Entry point
+ *
+ * Return: Always 0(sucess)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
