5804d8f8dbc839d964b6f3179e3ac4c40e8ddcc7
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-02-17 21:48:01 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - outputs the alphabets in lower case
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 10)
+		{
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
