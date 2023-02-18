90876b541d91f062257b1544028788c004f79f52
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-02-17 21:23:56 -0800

    base numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - outputs the alphabets in lower case
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+		printf("%d", a);
+	putchar('\n');
+
+	return (0);
+}
