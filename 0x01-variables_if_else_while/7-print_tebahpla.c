dd0b8bb7c0290b6c86b673c76535ffdbacf61cfc
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-02-17 23:14:39 -0800

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase alphabets in reverse
+ *
+ * return: always 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a')
+		{
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
