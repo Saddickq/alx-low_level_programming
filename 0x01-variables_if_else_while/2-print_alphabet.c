a81207713efcdd7dc3b18710ab19b0edd9bd0746
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-02-17 20:50:42 -0800

    mixed alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	char alpha[26] = "abcdefghijklmnopqrstuvqxyz";
+	char alp[26] = "abcdefghijklmnopqrstuvqxyz";
 	int i;
 
 	for (i = 0; i > 26; i++)
 	{
-		putchar(alpha[i]);
+		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - outputs the alphabets in lower case
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char a;
+	char d;
+
+	a = 'a';
+	d = 'A';
+	while
+		(a <= 'z')
+		{
+			putchar(a);
+			a++;
+		}
+	while
+		(d <= 'Z')
+		{
+			putchar(d);
+			d++;
+		}
+	return (0);
+}
