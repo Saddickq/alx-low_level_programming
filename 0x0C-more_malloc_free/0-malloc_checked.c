6c5cef2d413f57a1858f60513de68079894f7693
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-04-16 22:54:56 -0700

    0. Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory using malloc
+ * @b : unsigned int
+ * Return: nothing
+ */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *ptr;
+
+	ptr = (unsigned int *)malloc(sizeof(*ptr) * b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
+
