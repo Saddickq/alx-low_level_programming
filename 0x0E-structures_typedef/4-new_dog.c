bb8513342a4da5f2b700f8b89f3d4b0d127258db
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-04-18 21:19:14 -0700

    2. A dog will teach you unconditional love. If you can have that in your life, things won't be too bad

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - new dog
+ * @name : name
+ * @age : age
+ * @owner: owner
+ * Return:pointer
+ */
+dog_t *new_dog(char *name, float age, char *owner)
