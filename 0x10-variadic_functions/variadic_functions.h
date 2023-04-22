dfc470c7b6401f4ee5cc4cb96f90cdbddd2da044
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-04-21 21:32:20 -0700

    header file and readme

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,11 @@
+#ifndef "variadic_functions.h"
+#define "variadic_functions.h"
+#include <stddef.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int _putchaar(char c);
+
+#endif
