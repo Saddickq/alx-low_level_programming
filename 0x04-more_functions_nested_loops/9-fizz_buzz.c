5c2895151e318758242e9a5c6cc9d91f320b5363
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-04-25 00:24:59 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,25 +15,20 @@ int main(void)
 	{
 		if (number % 3 == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 		else if (number % 5 == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 		else if (number % 15 == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else
 		{
-			printf("%d", number);
-		}
-		if (number < 100)
-		{
-			printf(" ");
+			printf("%d ", number);
 		}
 	}
 	printf("\n");
 	return (0);
-}
