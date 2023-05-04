1e4c01576efe14102376e1879f876f2b2e7dd8c7
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-05-04 17:30:22 +0000

    1.1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,24 +3,30 @@
 /**
  * print_binary - convert decimal to binary
  * @n : number to convert
- * Return: binary
+ * Return: void
  */
 
 void print_binary(unsigned long int n)
 {
-	unsigned int mask = 1;
+	int flag = 0;
 
-	while ((mask << 1) <= n)
+	if (n == 0)
 	{
-		mask <<= 1;
+		_putchar(48);
+		return;
 	}
-	while (mask > 0)
+	int bits = sizeof(unsigned long int) * 8;
+
+	while (bits > 0)
 	{
-		if (mask & n)
-			_putchar('1');
-		else
-			_putchar('0');
-		mask >>= 1;
+		n <<= 1;
+		bits--;
+		if ((n & 1) == 1)
+			flag = 1;
+		if (flag)
+			if ((n & 1) == 1)
+				_putchar(49);
+			else
+				_putchar(48);
 	}
-	_putchar('\n');
 }
