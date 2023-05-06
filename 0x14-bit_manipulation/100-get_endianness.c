5169f792615cecdf046a95aa24f0f4b9d6d770fc
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-05-06 04:28:18 +0000

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * get_endianness - get endiannes of machine
+ * Return: 1 if little and 0 if big endian
+ */
+
+int get_endianness(void)
+{
+    unsigned int i = 1;
+
+    char *ptr = (char *)&i;
+
+    if (*ptr)
+    {
+        return 1;
+    }
+    else
+        return 0;  // Big endian
+}
