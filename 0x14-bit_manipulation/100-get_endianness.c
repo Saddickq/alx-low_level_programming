c35f58f69c6918bd89a3746fd3f540c35382117e
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-05-05 22:46:30 -0700

    comply to betty

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,17 +3,16 @@
  * get_endianness - get endiannes of machine
  * Return: 1 if little and 0 if big endian
  */
-
 int get_endianness(void)
 {
-    unsigned int i = 1;
+	unsigned int i = 1;
 
-    char *ptr = (char *)&i;
+	char *ptr = (char *)&i;
 
-    if (*ptr)
-    {
-        return 1;
-    }
-    else
-        return 0;
+	if (*ptr)
+	{
+		return (1);
+	}
+	else
+		return (0);
 }
