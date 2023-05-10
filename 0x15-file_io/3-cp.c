a78350ea1cc458694fe86f53acd6dfda97e809f8
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-05-09 22:30:07 -0700

    3. cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -37,16 +37,12 @@ int main(int argc, char *argv[])
 	ssize_t nread;
 
 	if (argc != 3)
-	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
-	}
 	file_from = open(argv[1], O_RDONLY);
-	if (file_from <0)
-	{
+	if (file_from < 0)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
-	}
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, PERMS);
 	if (file_to < 0)
 	{
@@ -54,12 +50,12 @@ int main(int argc, char *argv[])
 		close_fd(file_from);
 		exit(99);
 	}
-	while((nread = read(file_from, buffer, BUFFER_SIZE)) > 0)
+	while ((nread = read(file_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		if (write(file_to, buffer, nread) != nread)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-	  		close_fd(file_from);
+			close_fd(file_from);
 			close_fd(file_to);
 			exit(99);
 		}
