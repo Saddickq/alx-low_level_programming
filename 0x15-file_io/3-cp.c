1bf3021e9c3a12e14cd2016fe248c1618fc09620
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-05-09 22:42:10 -0700

    copy files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -37,12 +37,16 @@ int main(int argc, char *argv[])
 	ssize_t nread;
 
 	if (argc != 3)
+	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
+	}
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from < 0)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
+	}
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, PERMS);
 	if (file_to < 0)
 	{
