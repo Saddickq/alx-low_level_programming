6e31605c038236f4f4bc8271e0b6ec1f8ed1fa58
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-05-12 04:33:46 +0000

    2-append_text_to_file.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -41,14 +41,12 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from < 0)
+	if ((file_from = open(argv[1], O_RDONLY)) < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, PERMS);
-	if (file_to < 0)
+	if ((file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, PERMS)) < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close_fd(file_from);
