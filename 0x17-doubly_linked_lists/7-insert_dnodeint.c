47e19c692e75c0459ac62cf751ce581b1d4cc219
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-06-16 14:17:12 -0700

    insetion correction

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,7 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		node = add_dnodeint(h ,n);
+		node = add_dnodeint(h, n);
 		return (node);
 	}
 	if (idx == len)
