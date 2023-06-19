a57f044784433298da3292668de59a3c4bd39cae
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-06-19 11:53:13 +0000

    creat a dynamic library

---
+++ 0x18-dynamic_libraries/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _putchar - print a character
+ * @c : character to print
+ * Return: int
+ */
+
+int _putchar(char c)
+{
+	return (putchar(c));
+}
